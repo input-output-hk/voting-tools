65fdbdb813bb671389c77887d61a5042541bcddf
Samuel Evans-Powell <mail@sevanspowell.net>
2022-05-18 13:32:56 +0800

    Remove unused Jormungandr module

--- test/Main.hs
+++ test/Main.hs
@@ -1,7 +1,6 @@
 import           Test.Tasty (TestTree, defaultMain, testGroup)
 
 import qualified Test.Cardano.API.Extended
-import qualified Test.Cardano.API.Jormungandr
 import qualified Test.Cardano.CLI.Voting.Metadata
 import qualified Test.Cardano.Catalyst.Crypto
 import qualified Test.Cardano.Catalyst.Presentation
@@ -14,7 +13,6 @@ tests = do
   testGroup "Unit tests"
     [ Test.Cardano.CLI.Voting.Metadata.tests
     , Test.Cardano.API.Extended.tests
-    , Test.Cardano.API.Jormungandr.tests
     , Test.Cardano.Catalyst.Presentation.tests
     , Test.Cardano.Catalyst.Crypto.tests
     ]
