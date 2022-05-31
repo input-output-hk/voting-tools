3e226be2597c5fe523862495602cc042c7c4b4b9
Samuel Evans-Powell <mail@sevanspowell.net>
2022-05-31 17:40:59 +0800

    Add tests for VotePower module

--- test/Main.hs
+++ test/Main.hs
@@ -3,6 +3,7 @@ import           Test.Tasty (TestTree, defaultMain, testGroup)
 import qualified Test.Cardano.API.Extended
 import qualified Test.Cardano.Catalyst.Crypto
 import qualified Test.Cardano.Catalyst.Registration
+import qualified Test.Cardano.Catalyst.VotePower
 
 main :: IO ()
 main = defaultMain tests
@@ -12,5 +13,6 @@ tests = do
   testGroup "Unit tests"
     [ Test.Cardano.Catalyst.Registration.tests
     , Test.Cardano.API.Extended.tests
+    , Test.Cardano.Catalyst.VotePower.tests
     , Test.Cardano.Catalyst.Crypto.tests
     ]
