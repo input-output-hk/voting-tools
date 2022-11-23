0fced7ce5a1a0d67814c6681779f1a39764a6655
Robin Stumm <robin.stumm@iohk.io>
2022-11-23 22:23:23 +0100

    add std flake and tullia

--- flake.nix
+++ flake.nix
@@ -16,9 +16,10 @@
     cardano-db-sync = {
       url = "github:input-output-hk/cardano-db-sync?ref=refs/tags/12.0.2";
     };
+    tullia.url = "github:input-output-hk/tullia";
   };
 
-  outputs = { self, nixpkgs, utils, haskellNix, iohkNix, cardano-db-sync, ... } @ inputs:
+  outputs = { self, nixpkgs, utils, haskellNix, iohkNix, cardano-db-sync, tullia, ... } @ inputs:
     let
       inherit (nixpkgs) lib;
       inherit (lib) head systems mapAttrs recursiveUpdate mkDefault
@@ -42,72 +43,81 @@
         (import ./nix/pkgs.nix)
       ];
 
-      mkHydraJobs = system:
-        lib.recursiveUpdate self.packages.${system} self.checks.${system} // {
+      mkHydraJobs = system: let
+        jobs = recursiveUpdate self.packages.${system} self.checks.${system} // {
           nixosTests = import ./nix/nixos/tests/default.nix {
             inherit system inputs;
             pkgs = self.legacyPackages.${system};
           };
         };
-
+      in
+        jobs // {
+          required = with nixpkgs.legacyPackages.${system}; releaseTools.aggregate {
+            name = "github-required";
+            meta.description = "All jobs required to pass CI";
+            constituents = lib.collect lib.isDerivation jobs ++ lib.singleton
+              (writeText "forceNewEval" self.rev or "dirty");
+          };
+        };
     in
-      recursiveUpdate
-        # System-dependent jobs
-        (eachSystem supportedSystems (system:
-          let
-            pkgs = import nixpkgs {
-              inherit system overlays;
-              inherit (haskellNix) config;
-            };
+      eachSystem supportedSystems (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system overlays;
+            inherit (haskellNix) config;
+          };
 
-            flake = pkgs.votingToolsHaskellProject.flake {
-              crossPlatforms = p: with p; [
-                musl64
-              ];
-            };
-            packages = collectExes flake.packages // {
-              voterRegistrationTarball = pkgs.runCommandNoCC "voter-registration-tarball" { buildInputs = [ pkgs.gnutar pkgs.gzip ]; } ''
-                cp ${flake.packages."x86_64-unknown-linux-musl:voter-registration:exe:voter-registration"}/bin/voter-registration ./
-                mkdir -p $out/nix-support
-                tar -czvf $out/voter-registration.tar.gz voter-registration
-                echo "file binary-dist $out/voter-registration.tar.gz" > $out/nix-support/hydra-build-products
-              '';
+          flake = pkgs.votingToolsHaskellProject.flake {
+            crossPlatforms = p: with p; [
+              musl64
+            ];
+          };
+          packages = collectExes flake.packages // {
+            voterRegistrationTarball = pkgs.runCommandNoCC "voter-registration-tarball" { buildInputs = [ pkgs.gnutar pkgs.gzip ]; } ''
+              cp ${flake.packages."x86_64-unknown-linux-musl:voter-registration:exe:voter-registration"}/bin/voter-registration ./
+              mkdir -p $out/nix-support
+              tar -czvf $out/voter-registration.tar.gz voter-registration
+              echo "file binary-dist $out/voter-registration.tar.gz" > $out/nix-support/hydra-build-products
+            '';
 
-              votingToolsTarball = pkgs.runCommandNoCC "voting-tools-tarball" { buildInputs = [ pkgs.gnutar pkgs.gzip ]; } ''
-                cp ${flake.packages."x86_64-unknown-linux-musl:voting-tools:exe:voting-tools"}/bin/voting-tools ./
-                mkdir -p $out/nix-support
-                tar -czvf $out/voting-tools.tar.gz voting-tools
-                echo "file binary-dist $out/voting-tools.tar.gz" > $out/nix-support/hydra-build-products
-              '';
-            };
+            votingToolsTarball = pkgs.runCommandNoCC "voting-tools-tarball" { buildInputs = [ pkgs.gnutar pkgs.gzip ]; } ''
+              cp ${flake.packages."x86_64-unknown-linux-musl:voting-tools:exe:voting-tools"}/bin/voting-tools ./
+              mkdir -p $out/nix-support
+              tar -czvf $out/voting-tools.tar.gz voting-tools
+              echo "file binary-dist $out/voting-tools.tar.gz" > $out/nix-support/hydra-build-products
+            '';
+          };
 
-          in recursiveUpdate flake {
+        in recursiveUpdate flake {
 
-            inherit packages;
+          inherit packages;
 
-            legacyPackages = pkgs;
+          legacyPackages = pkgs;
 
-            devShells.stylish = pkgs.mkShell { packages = with pkgs; [ stylish-haskell git ]; };
+          devShells.stylish = pkgs.mkShell { packages = with pkgs; [ stylish-haskell git ]; };
 
-            # Built by `nix build .`
-            defaultPackage = flake.packages."voting-tools:exe:voting-tools";
+          # Built by `nix build .`
+          defaultPackage = flake.packages."voting-tools:exe:voting-tools";
 
-            # Run by `nix run .`
-            defaultApp = flake.apps."voting-tools:exe:voting-tools";
+          # Run by `nix run .`
+          defaultApp = flake.apps."voting-tools:exe:voting-tools";
 
-            inherit (flake) apps;
-
-            hydraJobs = mkHydraJobs system;
-          }
-        )
-      )
-    # Non-system-dependent jobs
-    {
-      hydraJobs.required = with self.legacyPackages.${lib.head supportedSystems}; releaseTools.aggregate {
-        name = "github-required";
-        meta.description = "All jobs required to pass CI";
-        constituents = lib.collect lib.isDerivation (mkHydraJobs (lib.head supportedSystems)) ++ lib.singleton
-          (writeText "forceNewEval" self.rev or "dirty");
+          inherit (flake) apps;
+        } //
+          tullia.fromSimple system (import nix/tullia.nix)
+      ) // {
+        # We can't use flake-utils' eachSystem for this
+        # because, unexpectedly, it outputs a different structure for hydraJobs.
+        hydraJobs = let
+          perSystem = lib.genAttrs supportedSystems mkHydraJobs;
+        in perSystem // {
+          required = with nixpkgs.legacyPackages.${defaultSystem}; releaseTools.aggregate {
+            name = "github-required";
+            meta.description = "All jobs required to pass CI";
+            constituents =
+              map (s: s.required) (__attrValues perSystem) ++
+              lib.singleton (writeText "forceNewEval" self.rev or "dirty");
+          };
+        };
       };
-    };
 }
